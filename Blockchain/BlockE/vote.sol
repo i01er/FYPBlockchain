9e2e1b8a951da00c41e42f178c6e2e30d0f73d7c
i01er <free2737@gmail.com>
2018-04-25 02:52:44 +0800

    Update vote.sol
    With 3 question

--- Blockchain/BlockE/vote.sol
+++ Blockchain/BlockE/vote.sol
@@ -16,26 +16,47 @@ contract Election
     struct Voter
     {
         bool voted;
-        uint voteIndex;
+        uint voteIndex1;
+        uint voteIndex2;
+        uint voteIndex3;
         uint weight;
     }
     
     address public owner;
     string public choice;
     mapping(address => Voter) public voters;
-    Question[] public questions;
+    Question[] public question1;
+    Question[] public question2;
+    Question[] public question3;
     uint public auctionEnd;
+
+    string public answer1 = "1 - Very Good";
+    string public answer2 = "2 - Okay";
+    string public answer3 = "3 - Not Good";
+    string public answer4 = "4 - Really Bad";
     
-    event QuestionResult(string choice, uint voteCount);
+    event Result(string choice, uint voteCount);
     
-    function Election(string _choice, uint durationMinutes, string choice1, string choice2)
+    function Election(string _choice, uint durationMinutes)
     {
         owner = msg.sender;
         choice = _choice;
         auctionEnd = now + (durationMinutes * 1 minutes);
         
-        questions.push(Question(choice1, 0));
-        questions.push(Question(choice2, 0));
+        question1.push(Question(answer1, 0));
+        question1.push(Question(answer2, 0));
+        question1.push(Question(answer3, 0));
+        question1.push(Question(answer4, 0));
+
+        question2.push(Question(answer1, 0));
+        question2.push(Question(answer2, 0));
+        question2.push(Question(answer3, 0));
+        question2.push(Question(answer4, 0));
+
+        question3.push(Question(answer1, 0));
+        question3.push(Question(answer2, 0));
+        question3.push(Question(answer3, 0));
+        question3.push(Question(answer4, 0));
     }
     
     function authorize(address voter)
@@ -46,15 +67,19 @@ contract Election
         voters[voter].weight = 1;
     }
     
-    function vote(uint voteIndex)
+    function vote(uint voteIndex1, uint voteIndex2, uint voteIndex3)
     {
         require(now < auctionEnd);
         require(!voters[msg.sender].voted);
         
         voters[msg.sender].voted = true;
-        voters[msg.sender].voteIndex = voteIndex;
+        voters[msg.sender].voteIndex1 = voteIndex1;
+        voters[msg.sender].voteIndex2 = voteIndex2;
+        voters[msg.sender].voteIndex3 = voteIndex3;
         
-        questions[voteIndex].voteCount += voters[msg.sender].weight;
+        question1[voteIndex1].voteCount += voters[msg.sender].weight;
+        question2[voteIndex2].voteCount += voters[msg.sender].weight;
+        question3[voteIndex3].voteCount += voters[msg.sender].weight;
     }
     
     function end()
@@ -64,7 +89,9 @@ contract Election
         
         for(uint i=0; i < questions.length; i++)
         {
-            QuestionResult(questions[i].choice, questions[i].voteCount);
+            Result(question1[i].choice, question1[i].voteCount);
+            Result(question2[i].choice, question2[i].voteCount);
+            Result(question3[i].choice, question3[i].voteCount);
         }
     }
 }
